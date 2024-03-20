fc9a79d0ae37e89ae091ececb77d6a99ea2e1293
KASRA10 <kasrasparrow@gmail.comm>
2024-03-20 12:05:26 +0330

    Add new simple code- Numbers with python

---
+++ Sequence Of Numbers.py
@@ -0,0 +1,9 @@
+for rows in range(1, 6):
+    for numbers in range(1, rows + 1):
+        print(numbers, end=" ")
+    print("")  # This Provides A Line Between Rows
+# Vise Versa
+for x in range(5, 0, -1):
+    for y in range(x, 0, -1):
+        print(y, end=" ")
+    print("")  # This Provides A Line Between Rows
